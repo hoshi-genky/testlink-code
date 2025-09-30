63edbbecca20bb48db4ac2f94a4d6a28749c5d21
noritaka-hoshi4201 <noritaka-hoshi4201@genky.co.jp>
2025-09-30 17:14:20 +0900

    [fix] テスト実行時にアップロードしたファイルが取得できない問題の修正

--- lib/attachments/attachmentdownload.php
+++ lib/attachments/attachmentdownload.php
@@ -97,6 +97,12 @@ if ($args->id) {
                     ON TPL.id = E.testplan_id
                     WHERE E.id = {$attParent}"; 
             $rs = $db->get_recordset($sql);
+            // 不具合修正 https://it-redmine.genky-aws.com/issues/13735
+            $ctx = array('tproject_id' => $rs[0]['testproject_id'],
+                         'tplan_id' => $rs[0]['testplan_id'],
+                         'checkPublicPrivateAttr' => true);
+            $ck = array('testplan_execute','exec_ro_access',
+                        'exec_testcases_assigned_to_me');
           break;
 
           case 'tcversions':
